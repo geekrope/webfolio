170b19ac33cfbc40ecc3a01d12cff2cc65ce47ca
geekrope <geekrope@gmail.com>
2021-07-08 19:36:01 +0300

    Touch interface

--- Typescript/drawing.ts
+++ Typescript/drawing.ts
@@ -1075,45 +1075,37 @@ function StartApp() {
 
 function BindEvents() {
 	var cnvs = document.getElementById(id);
-	cnvs.onmousedown = MouseDown;
-	cnvs.onmouseup = MouseUp;
-	cnvs.onwheel = MouseWheel;
+	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+		|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+		cnvs.ontouchstart = TouchStart;
+		cnvs.ontouchstart = TouchEnd;
+	}
+	else {
+		cnvs.onmousedown = MouseDown;
+		cnvs.onmouseup = MouseUp;
+		cnvs.onwheel = MouseWheel;
+	}
 	window.onresize = () => {
 		Resize();
 	}
 }
 
 function MouseDown(ev: MouseEvent) {
-	if (!rotation.rotationEnded) {
-		return;
+	if (rotation.rotationEnded) {
+		rotation.mouseDown.x = ev.pageX;
+		rotation.mouseDown.y = ev.pageY;
 	}
-	rotation.mouseDown.x = ev.pageX;
-	rotation.mouseDown.y = ev.pageY;
 }
 
 function MouseUp(ev: MouseEvent) {
-	if (!rotation.rotationEnded) {
-		return;
-	}
-	if (rotation.mouseDown.x == ev.pageX && rotation.mouseDown.y == ev.pageY) {
-		return;
-	}
-	if (Math.abs(ev.pageX - rotation.mouseDown.x) > Math.abs(ev.pageY - rotation.mouseDown.y)) {
-		if (ev.pageX - rotation.mouseDown.x < 0) {
-			rotation.deltaX = -defaultDelta;
-			rotation.rotationEnded = false;
-			if (currentShape instanceof Cube) {
-				currentShape.SideIndex++;
-				GetCurrentReference(currentShape);
+	if (!(rotation.mouseDown.x == ev.pageX && rotation.mouseDown.y == ev.pageY) && rotation.rotationEnded) {
+		if (Math.abs(ev.pageX - rotation.mouseDown.x) > Math.abs(ev.pageY - rotation.mouseDown.y)) {
+			if (ev.pageX - rotation.mouseDown.x < 0) {
+				RotateCurrentShape(-1);
 			}
-		}
-		else {
-			rotation.deltaX = defaultDelta;
-			if (currentShape instanceof Cube) {
-				currentShape.SideIndex--;
-				GetCurrentReference(currentShape);
+			else {
+				RotateCurrentShape(1);
 			}
-			rotation.rotationEnded = false;
 		}
 	}
 }
@@ -1134,9 +1126,13 @@ function ClearLink() {
 }
 
 function MouseWheel(ev: WheelEvent) {
-	let rotationEnded = !(rotation.rotateT < 90 && rotation.deltaX != 0);
+	var direction = Math.round(ev.deltaY / Math.abs(ev.deltaY));
+	TranslateShapes(direction);
+}
+
+function TranslateShapes(direction: number) {
+	let rotationEnded = rotation.rotationEnded;
 	if (rotationEnded && translation.translateStart == translation.translateTarget) {
-		var direction = Math.round(ev.deltaY / Math.abs(ev.deltaY));
 		var target = translation.translateStart + direction * distBetweenCubes;
 		if (currentShapeIndex + direction >= 0 && currentShapeIndex + direction < standsCount) {
 			currentShapeIndex += direction;
@@ -1160,6 +1156,46 @@ function MouseWheel(ev: WheelEvent) {
 		}
 		else {
 			ClearLink();
+		}
+	}
+}
+
+function RotateCurrentShape(direction: number) {
+	rotation.deltaX = Math.sign(direction) * defaultDelta;
+	rotation.rotationEnded = false;
+	if (currentShape instanceof Cube) {
+		currentShape.SideIndex++;
+		GetCurrentReference(currentShape);
+	}
+}
+
+function TouchStart(ev: TouchEvent) {
+	if (!rotation.rotationEnded) {
+		return;
+	}
+	rotation.mouseDown.x = ev.touches[0].clientX;
+	rotation.mouseDown.y = ev.touches[0].clientY;
+}
+
+function TouchEnd(ev: TouchEvent) {
+	var x = ev.touches[0].clientX;
+	var y = ev.touches[0].clientY;
+	if (!(rotation.mouseDown.x == x && rotation.mouseDown.y == y) && rotation.rotationEnded) {
+		if (Math.abs(x - rotation.mouseDown.x) > Math.abs(y - rotation.mouseDown.y)) {
+			if (x - rotation.mouseDown.x < 0) {
+				RotateCurrentShape(-1);
+			}
+			else {
+				RotateCurrentShape(1);
+			}
+		}
+		else {
+			if (y - rotation.mouseDown.y > 0) {
+				RotateCurrentShape(-1);
+			}
+			else {
+				RotateCurrentShape(1);
+			}
 		}
 	}
 }
